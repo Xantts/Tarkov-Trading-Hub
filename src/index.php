44d2e32decc38e3958a38185725071ec3f7dfa16
DVSS <99610648+Lizzen@users.noreply.github.com>
2023-03-09 10:36:25 +0100

    Práctica 2

---
+++ src/index.php
@@ -0,0 +1,39 @@
+<?php
+  session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <link rel="stylesheet" href="../css/index.css" />
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Tarkov Trading Hub</title>
+  </head>
+
+  <body>
+
+    <img src="../img/Logo.PNG" alt="Logo" height="300" class="center" />
+    <h1>Tarkov trading hub</h1>
+
+    <fieldset>
+      <legend>
+        <strong class="links">Enlaces al resto de páginas</strong>
+      </legend>
+      <p><a href="inventario.php">Inventario</a></p>
+      <p><a href="">Mercado</a></p>
+      <p><a href="">Subastas</a></p>
+      <p><a href="">Comunidad</a></p>
+    </fieldset>
+
+    <p>
+      Tarkov Trading Hub es una plataforma en línea para el juego Escape from
+      Tarkov. Ofrece una solución eficiente para gestionar tu inventario y
+      participar en un mercado de comercio con otros jugadores. Con un sistema
+      seguro y fácil de usar, podrás intercambiar objetos con otros jugadores y
+      discutir sobre estrategias de tradeo en un chat comunitario, además podrás
+      participar en subastas organizadas por el sistema de items únicos.
+    </p>
+  </body>
+</html>
---
+++ src/procesarLogin.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+$usuariosValidos = [
+    "user" => "userpass",
+    "admin" => "adminpass"
+];
+
+$username = htmlspecialchars(trim(strip_tags($_POST["username"])));
+$password = $_POST["password"];
+
+if (isset($usuariosValidos[$username]) && $usuariosValidos[$username] === $password) {
+    $_SESSION["login"] = true;
+    $_SESSION["nombre"] = ($username === "admin") ? "Administrador" : "Usuario";
+    
+    if ($username === "admin") {
+        $_SESSION["esAdmin"] = true;
+    }
+
+    if (isset($_GET["next"])) {
+        $_SESSION["next"] = $_GET["next"];
+    }
+    else {
+        $_SESSION["next"] = "index.php";
+    }
+
+    header("Location: " . $_SESSION["next"]);
+}
+else {
+    echo "Error: nombre de usuario o contraseña incorrectos.";
+    header("refresh:3;url=login.php");  
+}
+?>
